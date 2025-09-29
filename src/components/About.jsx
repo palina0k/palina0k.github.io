936d6efaf8a9a7d76ea0c9b7848e7e26e41e8bb1
Palina Karzhenka <korzik.polina@gmail.com>
2025-09-28 19:57:07 -0700

    Refined most of the styles. Finished 'About' and most of 'Technical' pages.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import headshot from '../images/karzhenka_headshot.jpg';
-import linkedin from '../images/linkedin_logo.png';
+import linkedin from '../images/linkedin-icon.png';
 import github from '../images/github_logo.png';
 
 import '../styles/About.css';
@@ -8,23 +8,53 @@ import '../styles/About.css';
 export default function About() {
     return (
         <div className="about_container" id="about">
-            <img src={headshot} alt="Palina Karzhenka Headshot" className="headshot" />
+            <div className="headshot">
+                <img src={headshot} alt="Palina Karzhenka Headshot" />
+            </div>
             <div className="info_container">
                 <div className="split_half">
-                    <h2>About Me</h2>
-                    <h6>Text, multiple lines</h6>
-                </div>
-                <div className="split_half">
+                    <h3>About Me</h3>
+                    <h6>
+                        Driven by curiosity for how things work, I'm a third-year Computer Science student building the foundation for a career in software engineering.👩🏻‍💻 <br/><br/>
+                        My journey so far has been filled with hands-on experiences like:
+                        <ul>
+                            <li><strong>Building full-stack web applications</strong> with student tech clubs, such as #include and GDSC, transforming ideas into a functional experience.</li>
+                            <li><strong>Leading and ensuring operational success</strong> as a Computer Lab Supervisor, refining my skills in team coordination and problem-solving in a dynamic environment.</li>
+                        </ul>
+                        I'm eager to bring my blend of technical skill and leadership to create meaningful impact in my engineering career.
+                    </h6>
                     <h3>Education</h3>
                     <h5>University of California - Davis</h5>
-                    <h6>Computer Science and Engineering | GPA: 3.91/4.0</h6>
-                    <h6>Expected Graduation: June 2027</h6>
-                    <br/>
+                    <h6>BS, Computer Science and Engineering | GPA: 3.91/4.0<br/><br/>Expected Graduation: June 2027</h6>
+                </div>
+                <div className="split_half">
                     <h3>Recent Experiences</h3>
-                    <h6> Text, multiple lines (roles and location and times)</h6>
+                    <h6>
+                        <strong>Computer Room Supervisor</strong> - Computer Lab Management at UC Davis<br/>
+                        <i>April 2025 - Present</i>
+                        <br/>
+                        <br/>
+                        <strong>Computer Room Consultant</strong> - Computer Lab Management at UC Davis<br/>
+                        <i>Septembet 2024 - Present</i>
+                        <br/>
+                        <br/>
+                        <strong>Student Instructor</strong> - ECS 98F: The Missing CS Quarter, UC Davis<br/>
+                        <i>September 2024 - Present</i>
+                        <br/>
+                        <br/>
+                    </h6>
+                    <h4>Project Experience</h4>
+                    <h6>
+                        <strong>Software Developer</strong> - #include<br/>
+                        <i>October 2024 - Present</i>
+                        <br/>
+                        <br/>
+                        <strong>Web Developer</strong> - Google Developer Student Club (GDSC)<br/>
+                        <i>September 2023 - June 2024</i>
+                    </h6>
                     <br/>
-                    <Link target={"_blank"} to="https://www.linkedin.com/in/palina-karzhenka"><img src={linkedin} alt='LinkedIn Icon' /></Link>
-                    <Link target={"_blank"} to="https://github.com/palina0k"><img src={github} alt="GitHub Icon" /></Link>
+                    <Link target={"_blank"} to="https://www.linkedin.com/in/palina-karzhenka"><img src={linkedin} alt='LinkedIn Icon' id="linkedin" /></Link>
+                    <Link target={"_blank"} to="https://github.com/palina0k"><img src={github} alt="GitHub Icon" id="github" /></Link>
                 </div>
             </div>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 //import nav_logo from '../images/[name]';
-import linkedin from '../images/linkedin_logo.png';
+import linkedin from '../images/linkedin-icon.png';
 import github from '../images/github_logo.png';
 
 import '../styles/Header.css';
@@ -9,17 +9,17 @@ export default function Header() {
     return (
         <div id="header">
             <nav className="header_container">
-                <div className="header_logo">
-                    <a href="/">{/* logo image here <img src={nav_logo} alt='Palina Karzhenka Site Logo' /> */}</a>
-                </div>
+                {/* <div className="header_logo">
+                    <a href="/">{ logo image here <img src={nav_logo} alt='Palina Karzhenka Site Logo' /> }</a>
+                </div> */}
                 <div className="header_left">
-                    <Link to="/technical">TECHNICAL</Link>
-                    <Link to="/studio-art">STUDIO ART</Link>
-                    <Link to="/about">ABOUT</Link>
+                    <Link to="/technical" id="tech" ><h5>TECHNICAL</h5></Link>
+                    <Link to="/studio-art" id="art" ><h5>STUDIO ART</h5></Link>
+                    <Link to="/about" id="abt" ><h5>ABOUT</h5></Link>
                 </div>
                 <div className="header_right">
-                    <Link target={"_blank"} to="https://www.linkedin.com/in/palina-karzhenka"><img src={linkedin} alt='LinkedIn Icon' /></Link>
-                    <Link target={"_blank"} to="https://github.com/palina0k"><img src={github} alt="GitHub Icon" /></Link>
+                    <Link target={"_blank"} to="https://www.linkedin.com/in/palina-karzhenka"><img src={linkedin} alt='LinkedIn Icon' id="linkedin" /></Link>
+                    <Link target={"_blank"} to="https://github.com/palina0k"><img src={github} alt="GitHub Icon" id="github" /></Link>
                 </div>
             </nav>
         </div>
