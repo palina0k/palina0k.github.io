10f2c7b7d17924f874fbb65ddaf3518ea5250667
Palina Karzhenka <korzik.polina@gmail.com>
2025-09-30 19:35:20 -0700

    Fixed the links on technical page

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -11,10 +11,10 @@ export default function Coding() {
         <div className="coding" id="coding">
             <h2>Coding Projects</h2>
             <div className="projectGroup">
-                <CodingCard title={"Cinema World"} description={"A social platform to review movies."} image={filmreviews} linkUrl={"https://github.com/palina0k/FilmReviews/tree/main"} />
-                <CodingCard title={"Campus Connect"} description={"A social platform made for SFSU students to voice campus concerns."} image={campusconnect} linkUrl={"https://devpost.com/software/campusconnect-orenh0"} />
-                <CodingCard title={"Aggie Ville"} description={"An all-in-one apartment finder for Davis residents."} image={aggieville} linkUrl={"https://github.com/alexqise/AggieVille/tree/main"} />
-                <CodingCard title={"Where's Perry"} description={"A two-player maze game."} image={wheresperry} linkUrl={"https://github.com/anagha166/WheresPerryFinal"} />
+                <CodingCard title={"Cinema World"} description={"A social platform to review movies."} image={filmreviews} linkIURL={"https://github.com/palina0k/FilmReviews/tree/main"} />
+                <CodingCard title={"Campus Connect"} description={"A social platform made for SFSU students to voice campus concerns."} image={campusconnect} linkURL={"https://devpost.com/software/campusconnect-orenh0"} />
+                <CodingCard title={"Aggie Ville"} description={"An all-in-one apartment finder for Davis residents."} image={aggieville} linkURL={"https://github.com/alexqise/AggieVille/tree/main"} />
+                <CodingCard title={"Where's Perry"} description={"A two-player maze game."} image={wheresperry} linkURL={"https://github.com/anagha166/WheresPerryFinal"} />
             </div>
         </div>
     );
