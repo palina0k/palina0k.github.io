e8fadfa940ec194baaac1228e6ea06e33cf50de5
Palina Karzhenka <korzik.polina@gmail.com>
2025-12-05 14:49:20 -0800

    Addition of hand and mouth detection project

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -3,6 +3,7 @@ import filmreviews from '../images/filmreviews.png';
 import campusconnect from '../images/campusconnect.png';
 import aggieville from '../images/aggieville.png';
 import wheresperry from '../images/wheresperry.png';
+import detection from '../images/detection.png';
 
 import '../styles/Coding.css';
 
@@ -11,6 +12,7 @@ export default function Coding() {
         <div className="coding" id="coding">
             <h2>Coding Projects</h2>
             <div className="projectGroup">
+                <CodingCard title={"Hand and Mouth Detection"} description={"A fun Python project that uses computer vision"} image={detection} linkURL={"https://github.com/palina0k/Hand-Recognition"} />
                 <CodingCard title={"Cinema World"} description={"A social platform to review movies."} image={filmreviews} linkURL={"https://github.com/palina0k/FilmReviews/tree/main"} />
                 <CodingCard title={"Campus Connect"} description={"A social platform made for SFSU students to voice campus concerns."} image={campusconnect} linkURL={"https://devpost.com/software/campusconnect-orenh0"} />
                 <CodingCard title={"Aggie Ville"} description={"An all-in-one apartment finder for Davis residents."} image={aggieville} linkURL={"https://github.com/alexqise/AggieVille/tree/main"} />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css';
 export default function Footer (){
     return (
         <div className="footer_container" id="footer">
-            <h6>Designed & coded with 🩷 <br/><br/> Last updated September 2025</h6>
+            <h6>Designed & coded with 🩷 <br/><br/> Last updated December 2025</h6>
         </div>
     )
 }
